8ee61f32d54dddd8832c4c6e54e6dd2663c136ba
rajbhavesh <58482189+rajbhavesh@users.noreply.github.com>
2019-12-06 20:36:47 +0530

    Update install-kubernetes.sh

--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -32,3 +32,8 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Install the flannel networking plugin
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
+
+
+#Deploying the Kubernetes Dashboard UI
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta6/aio/deploy/recommended.yaml
+
