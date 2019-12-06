b42b98a8b92f38acf1d1a5c1bf6f707526bd4b9c
rajbhavesh <58482189+rajbhavesh@users.noreply.github.com>
2019-12-06 17:51:42 +0530

    Create install-kubernetes.sh
    This configuration file is related to create a single node cluster.

---
+++ install-kubernetes.sh
@@ -0,0 +1,34 @@
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+
+
+# Add the Docker Repository
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" 
+
+
+# Add the Kubernetes repository
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - 
+cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list 
+deb https://apt.kubernetes.io/ kubernetes-xenial main 
+EOF 
+
+
+# Install Docker, Kubeadm, Kubelet, and Kubectl 
+sudo apt-get update 
+sudo apt-get install -y docker-ce kubelet kubeadm kubectl 
+
+
+# Enable net.bridge.bridge-nf-call-iptables
+echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf 
+sudo sysctl -p 
+
+
+# Initialize the cluster and configure kubectl
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+# Install the flannel networking plugin
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
